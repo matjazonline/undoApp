3d148146c3114eceb5fe79135b2c7bad8a127184
Denko <denkomanceski@gmail.com>
2016-10-01 12:16:49 +0200

    UNDO_UPDATE handler bug fix (was braking adding more than 1 undo + updating more than one on blur)

--- src/app/AppState.ts
+++ src/app/AppState.ts
@@ -12,15 +12,15 @@ export class AppState {
 
   public static STATE_NAME_UNDO_LIST = "undoList";
   public static STATE_NAME_SELECTED_UNDO = "selectedUndo";
-  public static UNDO_GET_LIST:string = "undoList.getList";
-  public static UNDO_CREATE:string = "undoList.create";
-  public static UNDO_REMOVE:string = "undoList.remove";
-  public static UNDO_UPDATE:string = "undoList.update";
-  public static UNDO_SELECT:string = "selectedUndo.set";
+  public static UNDO_GET_LIST: string = "undoList.getList";
+  public static UNDO_CREATE: string = "undoList.create";
+  public static UNDO_REMOVE: string = "undoList.remove";
+  public static UNDO_UPDATE: string = "undoList.update";
+  public static UNDO_SELECT: string = "selectedUndo.set";
 
-  public appStateVO:Observable<AppStateVO>;
+  public appStateVO: Observable<AppStateVO>;
 
-  constructor(@Inject(AppStateService) private appStateService:AppStateService) {
+  constructor(@Inject(AppStateService) private appStateService: AppStateService) {
     //this.userService = new UserService(http);
     this.initActionHandlers(appStateService);
     this.appStateVO = appStateService.getAppState().map(appStateObj=> {
@@ -28,31 +28,40 @@ export class AppState {
     })
   }
 
-  initActionHandlers(appStateSer:AppStateService):void {
+  initActionHandlers(appStateSer: AppStateService): void {
 
-    appStateSer.registerStateActionHandler(AppState.UNDO_GET_LIST, (param, undosListState:any[])=> {
-      if(!undosListState || undosListState.length<1) {
-        return [{title: "app state", desc: "based on react \n faster when immutable values and OnPush"},{title: "testing", desc: "description"},{title: "security", desc: "description"},{title: "RxJS", desc: "description"},{title: "modules", desc: "description"},{title: "forms", desc: "description"},{title: "ng-cli", desc: "description"}];
+    appStateSer.registerStateActionHandler(AppState.UNDO_GET_LIST, (param, undosListState: any[])=> {
+      if (!undosListState || undosListState.length < 1) {
+        return [{
+          title: "app state",
+          desc: "based on react \n faster when immutable values and OnPush"
+        }, {title: "testing", desc: "description"}, {title: "security", desc: "description"}, {
+          title: "RxJS",
+          desc: "description"
+        }, {title: "modules", desc: "description"}, {title: "forms", desc: "description"}, {
+          title: "ng-cli",
+          desc: "description"
+        }];
       }
     }, AppState.STATE_NAME_UNDO_LIST, null);
 
-    appStateSer.registerStateActionHandler(AppState.UNDO_CREATE, (undoObj, undosListState:any[])=> {
+    appStateSer.registerStateActionHandler(AppState.UNDO_CREATE, (undoObj, undosListState: any[])=> {
 
       let newUndosList = [undoObj, ...undosListState];
-      this.newAction(AppState.UNDO_SELECT,undoObj);
+      this.newAction(AppState.UNDO_SELECT, undoObj);
       return newUndosList;
 
     }, AppState.STATE_NAME_UNDO_LIST, null);
 
-    appStateSer.registerStateActionHandler(AppState.UNDO_REMOVE, (undoObj, undosListState:any[])=> {
+    appStateSer.registerStateActionHandler(AppState.UNDO_REMOVE, (undoObj, undosListState: any[])=> {
 
       /*if(this.selectedUndo===undo){
-        this.selectedUndo = null;
-      }*/
-      var removeFromIndex:number = undosListState.findIndex((undo)=>{
+       this.selectedUndo = null;
+       }*/
+      var removeFromIndex: number = undosListState.findIndex((undo)=> {
         return undo.title === undoObj.title;
       });
-      if(removeFromIndex>-1) {
+      if (removeFromIndex > -1) {
         undosListState.splice(removeFromIndex, 1);
       }
 
@@ -62,40 +71,43 @@ export class AppState {
 
     }, AppState.STATE_NAME_UNDO_LIST, null);
 
-    appStateSer.registerStateActionHandler(AppState.UNDO_UPDATE, (undoObj, undosListState:any[])=> {
-
-      var existingUndoIndex:number = undosListState.findIndex((undo)=>{
-        return undo.title === undoObj.title;
+    appStateSer.registerStateActionHandler(AppState.UNDO_UPDATE, (undoObj, undosListState: any[])=> {
+      console.log(undosListState, "?????");
+      var existingUndoIndex: number = undosListState.findIndex((undo)=> {
+        console.log(undo, undoObj, "obj");
+        return undo.title == undoObj.title;
       });
 
-      if(existingUndoIndex>-1) {
-        let existingUndo = undosListState[existingUndoIndex];
-        let newUndoWithUpdates = Object.assign( existingUndo, undoObj);
+      if (existingUndoIndex > -1) {
+        console.log(existingUndoIndex, "index.");
+        Object.assign(undosListState[existingUndoIndex], undoObj)
+        // let existingUndo = ;
+        // let newUndoWithUpdates = ;
         //let newUndoWithUpdates = Object.assign({}, existingUndo, undoObj);
         //undosListState.splice(existingUndoIndex, 1, newUndoWithUpdates);
         //return [...undosListState]
       }
-
+      return undosListState;
     }, AppState.STATE_NAME_UNDO_LIST, null);
 
-    appStateSer.registerStateActionHandler(AppState.UNDO_SELECT, (undoObj, currentlySelected:any)=> {
+    appStateSer.registerStateActionHandler(AppState.UNDO_SELECT, (undoObj, currentlySelected: any)=> {
       return undoObj;
     }, AppState.STATE_NAME_SELECTED_UNDO, null);
 
 
   }
 
-  newAction(eventConst:string, value?:any, stateObjectId?:string):Promise<any> {
+  newAction(eventConst: string, value?: any, stateObjectId?: string): Promise<any> {
     return this.appStateService.newAction(new StateAction(eventConst, value, stateObjectId));
   }
 
 }
 
 export class AppStateVO {
-  public undoList:any[];
+  public undoList: any[];
   public selectedUndo: any;
 
-  constructor(fromObj?:Object) {
+  constructor(fromObj?: Object) {
     if (fromObj) {
       Object.assign(this, fromObj);
     }
